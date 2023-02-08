8c7b5e40934d3dc815ad2334f1fbaddbe5374c79
maksim <paradoxxx47@gmail.com>
2023-02-08 19:22:15 +0600

    L6 - DI_2 + list set injection xml

---
+++ src/main/java/com/makos/spring/JazzMusic.java
@@ -0,0 +1,8 @@
+package com.makos.spring;
+
+public class JazzMusic implements Music{
+    @Override
+    public String getSong() {
+        return "Jazz song";
+    }
+}
--- src/main/java/com/makos/spring/MusicPlayer.java
+++ src/main/java/com/makos/spring/MusicPlayer.java
@@ -1,13 +1,42 @@
 package com.makos.spring;
 
-public class MusicPlayer {
-    private final Music music;
+import java.util.ArrayList;
+import java.util.List;
 
-    public MusicPlayer(Music music) {
-        this.music = music;
-    }
+public class MusicPlayer {
+    private List<Music> musicList = new ArrayList<>();
+
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {}
+
 
     public void playMusic() {
-        System.out.println("Playing: " + music.getSong());
+       musicList.forEach(music -> System.out.println("Playing: " + music.getSong()));
+    }
+
+    public List<Music> getMusicList() {
+        return musicList;
+    }
+
+    public void setMusicList(List<Music> musicList) {
+        this.musicList = musicList;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
