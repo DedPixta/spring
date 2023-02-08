225826b8b0610d7a3dab32a1659f509c0cd31f46
maksim <paradoxxx47@gmail.com>
2023-02-08 18:34:22 +0600

    created Spring app

---
+++ src/main/java/com/makos/spring/TestSpring.java
@@ -0,0 +1,15 @@
+package com.makos.spring;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext(
+                "applicationContext.xml"
+        );
+
+        TestBean testBean = context.getBean("testBean", TestBean.class);
+        System.out.println(testBean);
+    }
+}
