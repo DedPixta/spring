df1d03e105a0069bf035288f59fa3ab1d35f060a
Maxim Kossatyy <Paradoxxx47@gmail.com>
2023-02-09 20:31:10 +0600

    L6 MVC - controller with request parameters

--- src/main/java/com/makos/spring/controllers/FirstController.java
+++ src/main/java/com/makos/spring/controllers/FirstController.java
@@ -1,20 +1,30 @@
 package com.makos.spring.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/first")
 public class FirstController {
 
     @GetMapping("/hello")
-    public String helloPage(){
+    public String helloPage(@RequestParam("name") String name,
+                            @RequestParam(value = "surename", required = false) String surename){
+
+        System.out.println("hello, " + name + " " + surename);
         return "first/hello";
     }
 
     @GetMapping("/goodbye")
-    public String goodbyePage(){
+    public String goodbyePage(HttpServletRequest request){
+        String name = request.getParameter("name");
+        String surename = request.getParameter("surename");
+
+
+        System.out.println("buy, " + name + " " + surename);
         return "first/goodbye";
     }
 }
