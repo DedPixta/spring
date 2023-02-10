ff3421fff40ec767a71470cbb6c093456917d2ba
Maxim Kossatyy <Paradoxxx47@gmail.com>
2023-02-10 12:05:36 +0600

    L9 MVC - CRUD app (get/post for new person)

--- src/main/java/com/makos/spring/controllers/PeopleController.java
+++ src/main/java/com/makos/spring/controllers/PeopleController.java
@@ -4,9 +4,7 @@ import com.makos.spring.dao.PersonDAO;
 import com.makos.spring.models.Person;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -33,4 +31,14 @@ public class PeopleController {
         return "people/show";
     }
 
+    @GetMapping("/new")
+    public String newPerson(@ModelAttribute("person") Person person) {
+        return "people/new";
+    }
+
+    @PostMapping
+    public String create(@ModelAttribute("person") Person person) {
+        personDAO.save(person);
+        return "redirect:/people";
+    }
 }
--- src/main/java/com/makos/spring/dao/PersonDAO.java
+++ src/main/java/com/makos/spring/dao/PersonDAO.java
@@ -9,15 +9,16 @@ import java.util.List;
 @Component
 public class PersonDAO {
 
+    private static int PEOPLE_COUNT;
     private List<Person> people;
 
     {
         people = new ArrayList<>();
 
-        people.add(new Person(1, "Ivan"));
-        people.add(new Person(2, "Mariya"));
-        people.add(new Person(3, "Igor"));
-        people.add(new Person(4, "Andrii"));
+        people.add(new Person(++PEOPLE_COUNT, "Ivan"));
+        people.add(new Person(++PEOPLE_COUNT, "Mariya"));
+        people.add(new Person(++PEOPLE_COUNT, "Igor"));
+        people.add(new Person(++PEOPLE_COUNT, "Andrii"));
     }
 
     public List<Person> index() {
@@ -29,5 +30,10 @@ public class PersonDAO {
                 .filter(person -> person.getId() == id)
                 .findAny()
                 .orElse(null);
+    }
+
+    public void save(Person person) {
+        person.setId(++PEOPLE_COUNT);
+        people.add(person);
     }
 }
