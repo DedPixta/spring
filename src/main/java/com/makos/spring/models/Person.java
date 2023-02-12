b0ed7afca66843fb19b92db2e0c1272c3591fcd6
Maxim Kossatyy <Paradoxxx47@gmail.com>
2023-02-12 12:26:02 +0600

    L2 JDBC template - batch update

--- src/main/java/com/makos/spring/models/Person.java
+++ src/main/java/com/makos/spring/models/Person.java
@@ -4,7 +4,9 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 
+@Builder
 public class Person {
     private int id;
 
