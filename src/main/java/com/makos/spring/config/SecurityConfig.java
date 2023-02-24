cee406d8523582cd512a23d965ac1a9be02c6efc
Maxim Kossatyy <Paradoxxx47@gmail.com>
2023-02-24 21:20:02 +0600

    SprSec v6 - add @PreAuthorize method

--- src/main/java/com/makos/spring/config/SecurityConfig.java
+++ src/main/java/com/makos/spring/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
 
     private final SecurePersonDetailsService userDetailsService;
@@ -26,7 +28,6 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/auth/registration", "/error").permitAll()
                         .anyRequest().hasAnyRole("USER", "ADMIN")
                 )
--- src/main/java/com/makos/spring/controller/HelloController.java
+++ src/main/java/com/makos/spring/controller/HelloController.java
@@ -2,6 +2,8 @@ package com.makos.spring.controller;
 
 import com.makos.spring.model.SecurePerson;
 import com.makos.spring.security.SecurePersonDetails;
+import com.makos.spring.service.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -9,6 +11,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
     @GetMapping("/hello")
     public String printHello() {
@@ -26,6 +35,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage() {
+        adminService.doAdminStuff();
         return "admin";
     }
 }
